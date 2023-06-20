f0e335f21d07c056a7d7dedec44b9e869a2ec2fb
Matt Gates <matthew.gates2001@gmail.com>
2023-06-20 02:24:25 -0600

    Attempted ENV Fix. Hardcoded port, PSQL URL called from .env

--- server/controller.js
+++ server/controller.js
@@ -3,12 +3,7 @@ const {CONNECTION_STRING} = process.env
 const Sequelize = require('sequelize')
 
 const sequelize = new Sequelize(CONNECTION_STRING,{
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            rejectUnauthorized: false
-        }
-    }
+    dialect: 'postgres'
 })
 
 module.exports = {
