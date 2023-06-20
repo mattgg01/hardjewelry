b2dca91659b3bdc856ad2fdb5712ea3ded953e88
Matt Gates <matthew.gates2001@gmail.com>
2023-06-20 02:22:36 -0600

    Attempted ENV Fix. Hardcoded port, PSQL URL called from .env

--- server/index.js
+++ server/index.js
@@ -3,7 +3,6 @@ const app = express()
 require('dotenv').config()
 app.use(express.json())
 const path = require('path')
-const {SERVER_PORT} = process.env
 // const {seed} = require('./seed.js')
 const {concepts, conceptsCss, blackjack, blackjackCss, blackjackJs, conceptPost, editConcept, submitEdits, landing, landingCss, conceptsJs, getConcepts} = require('./controller.js')
 
@@ -24,4 +23,4 @@ app.get('/getConcepts', getConcepts)
 //To seed DB again, move 'seed.js' to server folder first
 
 
-app.listen(SERVER_PORT, () => console.log(`Server listening on port ${SERVER_PORT}`))
+app.listen(4000, () => console.log(`Server listening on port 4000`))
